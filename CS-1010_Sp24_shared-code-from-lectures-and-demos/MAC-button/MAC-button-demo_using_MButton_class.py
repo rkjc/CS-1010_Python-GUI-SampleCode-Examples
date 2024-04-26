6eb3f4872197098d54ba000e12d1d2cea0b4b2ca
rkjc <richardkjcross@gmail.com>
2024-04-26 11:36:38 -0700

    updated macbutton

--- CS-1010_Sp24_shared-code-from-lectures-and-demos/MAC-button/MAC-button-demo_using_MButton_class.py
+++ CS-1010_Sp24_shared-code-from-lectures-and-demos/MAC-button/MAC-button-demo_using_MButton_class.py
@@ -1,13 +1,15 @@
 # use your prefered way of importing and using the tkinter library
-import tkinter as tk
+
+import tkinter
 
 # by putting the macbutton.py file (contains the MButton class)
 # in the same folder as your python tkinter program it can be imported
 # without setting environment variables or using custom path
-import macbutton as mb
+
+import macbutton
 
 
-groot = tk.Tk()
+groot = tkinter.Tk()
 groot.geometry("400x300")
 
 count = 1
@@ -21,19 +23,17 @@ def do_butt_1(): # set mbutton color
     button_0.config(bg='orange')
 
 def do_butt_2(): # reset mbutton color
-    button_0.config(bg = mb.MButton.default_bg)
+    button_0.config(bg = macbutton.MButton.default_bg)
     
 
-button_0 = mb.MButton(groot, text='fake . . . . button', command=butt_action)
-print(button_0.default_bg)
-print(mb.MButton.default_bg)
+button_0 = macbutton.MButton(groot, text='fake button', command=butt_action)
 
-button_1 = tk.Button(groot, text='real button - set color', font = ('', 20), command=do_butt_1)
+button_1 = tkinter.Button(groot, text='real button - set color', font = ('', 20), command=do_butt_1)
 
-button_2 = tk.Button(groot, text='real button - clear color', font = ('', 20),command=do_butt_2)
+button_2 = tkinter.Button(groot, text='real button - clear color', font = ('', 20),command=do_butt_2)
 
 
-label_1 = tk.Label(groot, text="I'm a label", font = ('', 20), relief="sunken")
+label_1 = tkinter.Label(groot, text="I'm a label", font = ('', 20))
 label_1.pack()
 
 
@@ -44,3 +44,8 @@ button_2.pack()
 
 
 groot.mainloop()
+
+
+
+#print(button_0.default_bg)
+#print(mb.MButton.default_bg)
